1cffe6c9ec731c1c2e487790c5d31742c2b74638
MarcioAlex-x <marcioalex586@gmail.com>
2026-01-31 19:51:05 -0300

    change header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ export const Header = () =>{
 
     return(
         <header>
-            <h1 className={Style['title-header']}>Tech Blog</h1>
+            <h1 className={Style['title-header']}>TechN Blog</h1>
             <img className={Style['image-header']} src="/techblog.png" alt="Hero Tech Blog - O seu blog diário sobre tecnologia da informação." />
         </header>
     )
